1221d11b80f581dae183c673402d9c99b890e136
kennedy <kennedy.sagarino@alloc8.io>
2026-02-15 18:01:51 +0800

    update dummy orderlfow

--- app/api/order-flow/route.ts
+++ app/api/order-flow/route.ts
@@ -354,8 +354,10 @@
 //   );
 // }
 
+import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  /**to do */
-  return {}
+export async function GET(
+  _req: Request
+) {
+ return NextResponse.json({ error: "Not found" }, { status: 404 });
 }
